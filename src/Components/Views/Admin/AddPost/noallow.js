4d03f303254add6fb1be4d9a6696c8d73da6d3dd
Aleksander Dudek <aleksander.dudek@tieto.com>
2018-07-06 15:06:16 +0200

    added navigatorDrawer to notallow

--- src/Components/Views/Admin/AddPost/noallow.js
+++ src/Components/Views/Admin/AddPost/noallow.js
@@ -1,10 +1,55 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Navigation } from 'react-native-navigation';
+
+import { navigatorDrawer } from '../../../Utils/misc';
 
 class NotAllow extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.props.navigator.setOnNavigatorEvent((event)=>{
+            navigatorDrawer(event, this)
+        })
+
+    }
+
     render(){
         return(
-            <Text>Shall not pass</Text>
+            <View style={{
+                flex:1,
+                alignItems: 'center',
+                justifyContent:'center'
+            }}>
+            
+                <Icon 
+                    name="frown-o"
+                    size={60}
+                    color="#F44336"
+                />
+                <Text>
+                    You need to log in or register to sell !!!
+                </Text>
+                <Button 
+                    title="LOGIN / REGISTER"
+                    color="#FD9727"
+                    onPress={()=>{
+
+                        Navigation.startSingleScreenApp({
+                            screen:{
+                                screen:"sellItApp.Login",
+                                title:"Login",
+                                navigatorStyle:{
+                                    navBarHidden:true
+                                }
+                            }
+                        })
+
+                    }}
+                />
+            </View>
         )
     }
 }
